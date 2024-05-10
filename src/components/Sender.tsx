ad33cfc7d0b862e65bc3c7b589b0495a24384b61
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-10 09:46:17 +0530

    adding sender component

--- src/components/Sender.tsx
+++ src/components/Sender.tsx
@@ -1,7 +1,15 @@
-export const Sender = ()=>{
-    return(
-        <div>
-            sender
-        </div>
-    )
-}
+import { useEffect, useState } from "react"
+
+export const Sender = () => {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [pc, setPC] = useState<RTCPeerConnection | null>(null);
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:8080');
+        setSocket(socket);
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: 'sender'
+            }));
+        }
+    }, []);

aa965ae5bb52e9779cba10b37def6c6db2a99ec4
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-13 18:54:56 +0530

    adding receiver

--- src/components/Receiver.tsx
+++ src/components/Receiver.tsx
@@ -1,7 +1,14 @@
-export const Receiver = ()=>{
-    return(
-        <div>
-            reciever
-        </div>
-    )
-}
+import { useEffect } from "react"
+
+
+export const Receiver = () => {
+    
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:8080');
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: 'receiver'
+            }));
+        }
+        startReceiving(socket);
+    }, []);
